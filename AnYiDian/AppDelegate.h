f61a4b927bb8998c1eda4f12d6e036ce9c84d0d8
greenorangeCN <Seven@appledeMacBook-Pro.local>
2015-07-08 11:11:25 +0800

    Initial Commit

---
+++ AnYiDian/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnYiDian
+//
+//  Created by Seven on 15/7/8.
+//  Copyright (c) 2015年 Seven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
